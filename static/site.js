3b433f4f98ad2c8bfcc5de8e4fe402d24e4bd3e3
Alex Conner <hello@codatory.com>
2011-12-13 14:03:29 -0500

    Add Meetup.com API "stuff" for Upcoming page

---
+++ static/site.js
@@ -0,0 +1,37 @@
+$(function(){
+  var page_name = $('body').data('name');
+
+  if(page_name){
+    try{
+      eval('Site.'+page_name+'();');
+    } catch(e) {
+      Site.default();
+    }
+  } else {
+    Site.default();
+  }
+});
+
+var Site = {
+  default: function() {
+    console.log('No JS Action for this URL.');
+  },
+
+  upcoming: function() {
+    $.getJSON("http://api.meetup.com/2/events.json/?group_id=1366859&key=91526d36f317c59191cc632444&callback=?",
+    function (data) {
+      console.log(data);
+      var nextMeetup = data.results[0];
+      var date = new Date(nextMeetup.time);
+      var address = [nextMeetup.venue.address_1, nextMeetup.venue.address_2, nextMeetup.venue.city, nextMeetup.venue.state, nextMeetup.venue.zip].join(', ')
+      $('#next_meetup_name').html(nextMeetup.name);
+      $('#next_meetup_description').html(nextMeetup.description);
+      $('#next_meetup_address').html(nextMeetup.venue.name + ', ' + address);
+      $('#next_meetup_date').html(date.toLocaleString('%b %e, %l:%M %p'));
+      $('#next_meetup_rsvpcount').html(nextMeetup.yes_rsvp_count);
+      $('#next_meetup_event_url').attr('href', nextMeetup.event_url);
+
+      $('#next_meetup_venue_map').attr('src', 'http://maps.googleapis.com/maps/api/staticmap?sensor=false&size=500x400&markers='+address).show();
+    });
+  }
+}
--- upcoming.md
+++ upcoming.md
@@ -1,5 +1,25 @@
 ---
 layout: default
+name: upcoming
 ---
 
-Hook into meetup API here
+<h2 id="next_meetup_name">
+  Indy Software Artisans
+</h2>
+
+<p>
+  Our next meeting is <span id="next_meetup_date">Every 3rd Thursday at 6:00pm</span> at
+  <a href="#next_meetup_venue_map" id="next_meetup_address">SEP</a>.
+</p>
+
+<p>
+  So far, <span id="next_meetup_rsvpcount">unknown</span> people have RSVP'd. Secure your
+  seat and <a href="http://www.meetup.com/" id="next_meetup_event_url"> RSVP Today! </a>
+</p>
+
+<p id="next_meetup_description">
+  I haven't loaded the latest meetup's data from Meetup.com yet. Please wait a moment or
+  try again later.
+</p>
+
+<img src="" id="next_meetup_venue_map" alt="Map to Venue" style="display:none;"/>
