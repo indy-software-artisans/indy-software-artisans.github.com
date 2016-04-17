594495389d2efce848e7176727b145ac70f3e48d
Arktronic <arktronic@gmail.com>
2016-04-17 16:51:57 -0400

    Fixed issues with HTTPS, Meetup API, and a 404

--- static/site.js
+++ static/site.js
@@ -12,31 +12,38 @@ $(function(){
   }
 });
 
+function renderUpcoming(data) {
+  var nextMeetup = data.results[0];
+  var date = new Date(nextMeetup.time);
+  var dateString = date.getFullYear() + '-' + ('0' + (date.getMonth()+1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
+  var timeString = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
+  var address = [nextMeetup.venue.address_1, nextMeetup.venue.address_2, nextMeetup.venue.city, nextMeetup.venue.state].join(', ');
+  $('#next_meetup_name').html(nextMeetup.name);
+  $('#next_meetup_description').html(nextMeetup.description);
+  $('#next_meetup_address').html(nextMeetup.venue.name + ', ' + address);
+  $('#next_meetup_date').html(dateString + ' at ' + timeString);
+  $('#next_meetup_rsvpcount').html(nextMeetup.yes_rsvp_count);
+  $('#next_meetup_event_url').attr('href', nextMeetup.event_url);
+
+  var map = $('#next_meetup_venue_map');
+  map.attr('src', 'https://maps.googleapis.com/maps/api/staticmap?sensor=false&size=535x400&markers='+address);
+  map.parent('a').attr('href', 'https://maps.google.com/maps?daddr='+address);
+  map.parent('a').attr('target', '_blank')
+  map.show();
+}
+
 var Site = {
   default: function() {
     // Any JavaScript that needs to run on every page should go here.
   },
 
   upcoming: function() {
-    var signed_url = "//api.meetup.com/2/events.json/?group_id=1366859&status=upcoming&_=1323957798477&sig_id=7728671&order=time&desc=false&sig=79e04bfe18e4845882f65aebc76c89b33cfbaf71&offset=0&callback=?&format=json&page=200&fields=&sig_id=7728671&sig=73b75f3ee416fb0891de36fae02a9aed6cfd10d3"
-    $.getJSON(signed_url, function (data) {
-      var nextMeetup = data.results[0];
-      var date = new Date(nextMeetup.time);
-	  var dateString = date.getFullYear() + '-' + ('0' + (date.getMonth()+1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
-	  var timeString = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
-      var address = [nextMeetup.venue.address_1, nextMeetup.venue.address_2, nextMeetup.venue.city, nextMeetup.venue.state].join(', ');
-      $('#next_meetup_name').html(nextMeetup.name);
-      $('#next_meetup_description').html(nextMeetup.description);
-      $('#next_meetup_address').html(nextMeetup.venue.name + ', ' + address);
-      $('#next_meetup_date').html(dateString + ' at ' + timeString);
-      $('#next_meetup_rsvpcount').html(nextMeetup.yes_rsvp_count);
-      $('#next_meetup_event_url').attr('href', nextMeetup.event_url);
-
-      var map = $('#next_meetup_venue_map');
-      map.attr('src', 'http://maps.googleapis.com/maps/api/staticmap?sensor=false&size=535x400&markers='+address);
-      map.parent('a').attr('href', 'http://maps.google.com/maps?daddr='+address);
-      map.parent('a').attr('target', '_blank')
-      map.show();
+    var signed_url = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=indysa&callback=renderUpcoming&page=1&fields=&order=time&desc=false&status=upcoming&sig_id=13753510&sig=09e14b67a36e448ed2bc8af672a7d62d1a94aaa0"
+    $.ajax({
+      url: signed_url,
+      crossDomain: true,
+	  cache: true,
+	  dataType: 'script'
     });
   }
 }
